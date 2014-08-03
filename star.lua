713e0153aaa1c43df8a0db268f713c0b5dc922da
Joseph Gilley <joe.gilley@gmail.com>
2014-08-03 18:11:07 -0400

    Exposed some internal fields, collision filter, yellowness

--- star.lua
+++ star.lua
@@ -4,16 +4,26 @@ local json = require('json')
 
 local StarModule = {}
 
-local STAR_STANDARD_SIZE = 2
+local STAR_STANDARD_SIZE = 5
+local STAR_CATEGORY_BIT = 1
+local STAR_COLLIDE_BITS = 2
 
-local starDisplayGroup = display.newGroup()
+local STAR_GROUP = display.newGroup()
+
+StarModule.STAR_CATEGORY_BIT = STAR_CATEGORY_BIT
+StarModule.STAR_COLLIDE_BITS = STAR_COLLIDE_BITS
+StarModule.STAR_DISPLAY_GROUP = STAR_GROUP
+
+local starCounter = 1
 
 -- public methods
 function StarModule.new ( vals )
-    local star = Proxy.get(display.newRect(starDisplayGroup, vals.x or -10, vals.y or -10, vals.size or STAR_STANDARD_SIZE, vals.size or STAR_STANDARD_SIZE))
+    local star = Proxy.get(display.newRect(STAR_GROUP, vals.x or -10, vals.y or -10, vals.size or STAR_STANDARD_SIZE, vals.size or STAR_STANDARD_SIZE))
     star.color = vals.color or {1, 1, 1}
+    star.name = vals.name or "Star-" .. starCounter
+    if not vals.name then starCounter = starCounter + 1 end
     
-    local setupShiftColor, randomFlickerTimer
+    local getStarColor, randomFlickerTimer
 
     function star:init( vals )
         vals = vals or {}
@@ -21,15 +31,15 @@ function StarModule.new ( vals )
         self.y = vals.y or math.random(0, display.contentHeight)
 
         self.width = vals.width or (self.width + math.random(0, 3))
-        self.height = vals.height or (self.height + math.random(0, 3))
+        self.height = self.width
 
         -- TODO vals.color needs to be shallow copied or it could reference multiple tables
-        -- self.fill = vals.paint or setupShiftColor(self.fill, 2, 0.5)
+        -- self.fill = vals.paint or getStarColor(self.fill, 2, 0.5)
 
         randomFlickerTimer(self)
-        self.color = setupShiftColor(self.color, 2, 1.0, 0.7)
+        self.color = getStarColor(self.color, 2, 1.0, 0.7)
         self:setFillColor( self.color[1],  self.color[2], self.color[3] )
-        physics.addBody( self, "static", { isSensor = true } )
+        physics.addBody( self, "static", { isSensor = true, filter = { categoryBits = STAR_CATEGORY_BIT, maskBits = STAR_COLLIDE_BITS } } )
         self:addEventListener( "enterFrame", self )
     end
 
@@ -48,7 +58,7 @@ function StarModule.new ( vals )
         end)
     end
 
-    function setupShiftColor(initialColor, shiftProbabilityPower, shiftIntensity, shiftThreshold)
+    function getStarColor(initialColor, shiftProbabilityPower, shiftIntensity, shiftThreshold)
         -- Do a shallow copy so we don't mess up shared tables
         local color = {}
         color[1], color[2], color[3] = initialColor[1], initialColor[2], initialColor[3]
@@ -75,6 +85,9 @@ function StarModule.new ( vals )
                 color[1] = color[1] - shift
             end
         end
+
+        local yellowness = math.random(0, 3) / 10
+        color[3] = math.max(0, color[3] - yellowness)
         return color
     end
 
